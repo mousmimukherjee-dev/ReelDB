2916b12f38fd006a646664229377ac5bcfbba990
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-05-10 01:16:40 +0200

    add:header section

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,23 @@
+import { Icons } from 'next/dist/lib/metadata/types/metadata-types'
+import Link, { LinkProps } from 'next/link'
+import React from 'react'
+import { IconType } from 'react-icons'
+
+
+interface MenuItemProps{
+
+  title:string,
+  address:string,
+  Icon:IconType
+}
+
+const MenuItem = ({title,address,Icon}:MenuItemProps) => {
+  return (
+    <Link href={address} className='flex hover:text-amber-500'>
+      <Icon className='text-2xl sm:hidden'/>
+      <p className='uppercase hidden sm:inline text-sm'>{title}</p>
+    </Link>
+  )
+}
+
+export default MenuItem

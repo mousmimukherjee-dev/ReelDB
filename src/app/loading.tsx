387a66f13e0ec6e47176e97d9d1649d3ad88d776
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2026-05-11 14:08:35 +0200

    add loading effect using Next.js

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Loading(){
+
+
+  return(<div className="flex justify-center mt-16"><img src="spinner.svg" alt="loading..." className="h-52"/></div>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home({searchParams}:HomeProps){
 
   try{
 
-  const res = await fetch(`https://api.themoviedb.org/3${genre==="fetchTopRated"?`/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`);
+  const res = await fetch(`https://api.themoviedb.org/3${genre==="fetchTopRated"?`/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:10000}});
   if(!res.ok){
 
     throw new Error("Failed to fetch")
